7f5f43780afb0128270f1b5e89b312e28a933345
Nic Raboy <nraboy@gmail.com>
2015-06-28 06:53:26 -0700

    add gradle, but havent tested it yet

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-Ubuntu Ionic Installer
-======================
+[![PayPal](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XK2JBEZ2PD2QS)
+[![Square](https://img.shields.io/badge/square-donate-yellow.svg)](https://cash.me/$nraboy)
 
-Installs Android, Apache Ant, Java JDK, NodeJS, Apache Cordova, and Ionic Framework on Ubuntu x64 and x86 architectures.  Has been tested with Ubuntu 14.04 Trusty Tahr.
+
+# Ubuntu Ionic Installer
+
+Installs Android, Apache Ant, Gradle, Java JDK, NodeJS, Apache Cordova, and Ionic Framework on Ubuntu x64 and x86 architectures.  Has been tested with Ubuntu 14.04 Trusty Tahr.
 
 [http://blog.nraboy.com/2014/09/install-android-cordova-ionic-framework-ubuntu/](http://blog.nraboy.com/2014/09/install-android-cordova-ionic-framework-ubuntu/)
 
-Have a question or found a bug (compliments work too)?
--------------
+## Contribution Rules
 
-Email me via my website - [http://www.nraboy.com](http://www.nraboy.com/contact)
+All contributions must be made via the `development` branch.  This keeps the project more maintainable in terms of versioning as well as code control.
+
+## Have a question or found a bug (compliments work too)?
 
 Tweet me on Twitter - [@nraboy](https://www.twitter.com/nraboy)
 
-Resources
--------------
+## Resources
 
 Ionic Framework - [http://www.ionicframework.com](http://www.ionicframework.com)
 
@@ -22,3 +25,5 @@ NodeJS - [http://www.nodejs.org](http://www.nodejs.org)
 Apache Cordova - [http://cordova.apache.org](http://cordova.apache.org)
 
 Android - [http://developer.android.com](http://developer.android.com)
+
+Gradle - [https://www.gradle.org](https://www.gradle.org)
--- ubuntu_ionic_installer.sh
+++ ubuntu_ionic_installer.sh
@@ -17,17 +17,21 @@ HOME_PATH=$(cd ~/ && pwd)
 INSTALL_PATH=/opt
 ANDROID_SDK_PATH=/opt/android-sdk
 NODE_PATH=/opt/node
+GRADLE_PATH=/opt/gradle
 
 # x86_64 or i686
 LINUX_ARCH="$(lscpu | grep 'Architecture' | awk -F\: '{ print $2 }' | tr -d ' ')"
 
-# Latest Android Linux SDK for x64 and x86 as of 7-5-2015
-ANDROID_SDK_X64="http://dl.google.com/android/android-sdk_r24.2-linux.tgz"
-ANDROID_SDK_X86="http://dl.google.com/android/android-sdk_r24.2-linux.tgz"
+# Latest Android Linux SDK for x64 and x86 as of 06-28-2015
+ANDROID_SDK_X64="http://dl.google.com/android/android-sdk_r24.3.3-linux.tgz"
+ANDROID_SDK_X86="http://dl.google.com/android/android-sdk_r24.3.3-linux.tgz"
 
-# Latest NodeJS for x64 and x86 as of 7-5-2015
-NODE_X64="http://nodejs.org/dist/v0.12.2/node-v0.12.2-linux-x64.tar.gz"
-NODE_X86="http://nodejs.org/dist/v0.12.2/node-v0.12.2-linux-x86.tar.gz"
+# Latest NodeJS for x64 and x86 as of 06-28-2015
+NODE_X64="http://nodejs.org/dist/v0.12.2/node-v0.12.5-linux-x64.tar.gz"
+NODE_X86="http://nodejs.org/dist/v0.12.2/node-v0.12.5-linux-x86.tar.gz"
+
+# Latest Gradle as of 06-28-2015
+GRADLE_ALL="https://services.gradle.org/distributions/gradle-2.4-all.zip"
 
 if [ "$LINUX_ARCH" == "x86_64" ]; then
     # Add i386 architecture
@@ -43,12 +47,16 @@ if [ "$LINUX_ARCH" == "x86_64" ]; then
 
     wget "$NODE_X64" -O "nodejs.tgz"
     wget "$ANDROID_SDK_X64" -O "android-sdk.tgz"
+    wget "$GRADLE_ALL" -O "gradle.zip"
 
     tar zxf "nodejs.tgz" -C "$INSTALL_PATH"
     tar zxf "android-sdk.tgz" -C "$INSTALL_PATH"
+    unzip "gradle.zip"
+    mv "gradle-2.4" "$INSTALL_PATH"
 
     cd "$INSTALL_PATH" && mv "android-sdk-linux" "android-sdk"
-    cd "$INSTALL_PATH" && mv "node-v0.12.2-linux-x64" "node"
+    cd "$INSTALL_PATH" && mv "node-v0.12.5-linux-x64" "node"
+    cd "$INSTALL_PATH" && mv "gradle-2.4" "gradle"
 
     # Android SDK requires some x86 architecture libraries even on x64 system
     apt-get install -qq -y libc6:i386 libgcc1:i386 libstdc++6:i386 libz1:i386
@@ -57,12 +65,16 @@ else
 
     wget "$NODE_X86" -O "nodejs.tgz"
     wget "$ANDROID_SDK_X86" -O "android-sdk.tgz"
+    wget "$GRADLE_ALL" -O "gradle.zip"
 
     tar zxf "nodejs.tgz" -C "$INSTALL_PATH"
     tar zxf "android-sdk.tgz" -C "$INSTALL_PATH"
+    unzip "gradle.zip"
+    mv "gradle-2.4" "$INSTALL_PATH"
 
     cd "$INSTALL_PATH" && mv "android-sdk-linux" "android-sdk"
-    cd "$INSTALL_PATH" && mv "node-v0.12.2-linux-x86" "node"
+    cd "$INSTALL_PATH" && mv "node-v0.12.5-linux-x86" "node"
+    cd "$INSTALL_PATH" && mv "gradle-2.4" "gradle"
 
 fi
 
@@ -75,11 +87,13 @@ cd ~/
 echo "export PATH=\$PATH:$ANDROID_SDK_PATH/tools" >> ".profile"
 echo "export PATH=\$PATH:$ANDROID_SDK_PATH/platform-tools" >> ".profile"
 echo "export PATH=\$PATH:$NODE_PATH/bin" >> ".profile"
+echo "export PATH=\$PATH:$GRADLE_PATH/bin" >> ".profile"
 
 # Add Android and NPM paths to the temporary user path to complete installation
 export PATH=$PATH:$ANDROID_SDK_PATH/tools
 export PATH=$PATH:$ANDROID_SDK_PATH/platform-tools
 export PATH=$PATH:$NODE_PATH/bin
+export PATH=$PATH:$GRADLE_PATH/bin
 
 # Install JDK and Apache Ant
 apt-get -qq -y install default-jdk ant
@@ -95,10 +109,12 @@ npm install -g cordova
 npm install -g ionic
 
 cd "$INSTALL_PATH" && chmod 777 "node" -R
+cd "$INSTALL_PATH" && chmod 777 "gradle" -R
 
 # Clean up any files that were downloaded from the internet
 cd ~/Desktop && rm "android-sdk.tgz"
 cd ~/Desktop && rm "nodejs.tgz"
+cd ~/Desktop && rm "gradle.zip"
 
 echo "----------------------------------"
 echo "Restart your Ubuntu session for installation to complete..."
